ffe0fba6087bcfbf22d5b1baa9bb19a49dcd4436
Bundula Patrik <patrik.bundula@gammaorg.hu>
2024-01-13 19:35:21 +0100

    land page fetch the first 10 person

--- src/app/main/people-list/people-list.component.ts
+++ src/app/main/people-list/people-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map, tap } from 'rxjs';
+import { MainService } from 'src/app/core/services/main.service';
+import { PeopleResponseViewModel } from 'src/app/shared/models/PeopleResponseViewModel';
 
 @Component({
   selector: 'swapi-people-list',
   templateUrl: './people-list.component.html',
-  styleUrls: ['./people-list.component.scss']
+  styleUrls: ['./people-list.component.scss'],
 })
-export class PeopleListComponent {
+export class PeopleListComponent implements OnInit {
+  peopleList: PeopleResponseViewModel[] = [];
+  private pageIndex: number = 1;
 
+  constructor(private mainService: MainService) {}
+
+  ngOnInit(): void {
+    this.mainService.getPeople(this.pageIndex).subscribe((peopleList) => {
+      this.peopleList = peopleList;
+    });
+  }
 }

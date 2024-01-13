b18b66098dba1846cdbfa98baea1c1cff11fc42a
Bundula Patrik <patrik.bundula@gammaorg.hu>
2024-01-13 20:04:24 +0100

    connect parent and child component

--- src/app/main/people-list/list-item/list-item.component.ts
+++ src/app/main/people-list/list-item/list-item.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PeopleResponseViewModel } from 'src/app/shared/models/PeopleResponseViewModel';
 
 @Component({
   selector: 'swapi-list-item',
   templateUrl: './list-item.component.html',
-  styleUrls: ['./list-item.component.scss']
+  styleUrls: ['./list-item.component.scss'],
 })
 export class ListItemComponent {
-
+  @Input() peopleList: PeopleResponseViewModel[] = [];
 }

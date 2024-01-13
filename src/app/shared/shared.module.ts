1c8f94efe09edda2b2e36cd2ef006b1172c65d02
Bundula Patrik <patrik.bundula@gammaorg.hu>
2024-01-13 20:09:00 +0100

    land page layout add

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { NzListModule } from 'ng-zorro-antd/list';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 @NgModule({
   declarations: [LoaderComponent],
-  imports: [CommonModule, NzListModule, NzButtonModule],
-  exports: [LoaderComponent, NzListModule, NzButtonModule],
+  imports: [CommonModule, NzListModule, NzButtonModule, NzLayoutModule],
+  exports: [LoaderComponent, NzListModule, NzButtonModule, NzLayoutModule],
 })
 export class SharedModule {}

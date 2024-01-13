6ddd3a16d10d2d2863bb73523c1837c1d962b7f8
Bundula Patrik <patrik.bundula@gammaorg.hu>
2024-01-13 20:18:33 +0100

    add loading spinner logic

--- src/app/core/services/loader.service.ts
+++ src/app/core/services/loader.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoaderService {
+  private isLoading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    true
+  );
+  isLoading$: Observable<boolean> = this.isLoading.asObservable();
   constructor() {}
+
+  show(): void {
+    this.isLoading.next(true);
+  }
+
+  hide(): void {
+    this.isLoading.next(false);
+  }
 }

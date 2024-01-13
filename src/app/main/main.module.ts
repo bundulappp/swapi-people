c943427c6150897220e263ef56f13c2a95d987d7
Bundula Patrik <patrik.bundula@gammaorg.hu>
2024-01-13 20:04:40 +0100

    add basic display for list item

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { PeopleListComponent } from './people-list/people-list.component';
 import { ListItemComponent } from './people-list/list-item/list-item.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    MainComponent,
-    PeopleListComponent,
-    ListItemComponent
-  ],
-  imports: [
-    CommonModule,
-    MainRoutingModule
-  ]
+  declarations: [MainComponent, PeopleListComponent, ListItemComponent],
+  imports: [CommonModule, MainRoutingModule, SharedModule],
 })
-export class MainModule { }
+export class MainModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
-
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 @NgModule({
   declarations: [LoaderComponent],
-  imports: [CommonModule],
-  exports: [LoaderComponent],
+  imports: [CommonModule, NzListModule, NzButtonModule],
+  exports: [LoaderComponent, NzListModule, NzButtonModule],
 })
 export class SharedModule {}

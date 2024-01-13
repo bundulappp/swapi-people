a1ddafe97a86373b626c4e878170a8a884c0d47a
Bundula Patrik <patrik.bundula@gammaorg.hu>
2024-01-13 19:17:01 +0100

    add getPeople method

--- src/app/core/services/main.service.ts
+++ src/app/core/services/main.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PeopleDataResponseModel } from 'src/app/shared/models/PeopleDataResponseModel';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MainService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getPeople(pageIndex: number): Observable<PeopleDataResponseModel> {
+    return this.http.get<PeopleDataResponseModel>(
+      `${environment.apiUrl}/people/?page=${pageIndex}`
+    );
+  }
 }

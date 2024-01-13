80253aad7895febab5e8c62e6c530d6af2e0deb3
Bundula Patrik <patrik.bundula@gammaorg.hu>
2024-01-13 19:36:57 +0100

    add list-item  component

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { PeopleListComponent } from './people-list/people-list.component';
+import { ListItemComponent } from './people-list/list-item/list-item.component';
 
 
 @NgModule({
   declarations: [
     MainComponent,
-    PeopleListComponent
+    PeopleListComponent,
+    ListItemComponent
   ],
   imports: [
     CommonModule,

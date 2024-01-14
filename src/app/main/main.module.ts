42d76e11c64bee9fd9f17f68567f47beb9aed8af
Bundula Patrik <patrik.bundula@gammaorg.hu>
2024-01-14 08:16:39 +0100

    add search -bar component

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,9 +5,10 @@ import { MainComponent } from './main.component';
 import { PeopleListComponent } from './people-list/people-list.component';
 import { ListItemComponent } from './people-list/list-item/list-item.component';
 import { SharedModule } from '../shared/shared.module';
+import { SearchBarComponent } from './people-list/search-bar/search-bar.component';
 
 @NgModule({
-  declarations: [MainComponent, PeopleListComponent, ListItemComponent],
+  declarations: [MainComponent, PeopleListComponent, ListItemComponent, SearchBarComponent],
   imports: [CommonModule, MainRoutingModule, SharedModule],
 })
 export class MainModule {}
